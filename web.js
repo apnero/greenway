ff9937c1780ad36bf7d5f970e315660af2c05d4d
Ubuntu <ubuntu@ip-10-71-136-91.ec2.internal>
2013-12-12 18:28:27 +0000

    mods

--- web.js
+++ web.js
@@ -11,7 +11,7 @@ var postmark = require("postmark")("6d3c5ce6-1ce8-4906-b426-05eeba4fd1e2")
 app.post('/getFormData', function(req, res) {
   postmark.send({
     "From": "email@plasmascape.com",
-    "To": "inquire@plasmascape.com",
+    "To": "clayton@greenwaypropertycare.com",
     "Subject": "EMAIL FROM GREENWAY",
     "TextBody": new String('From: ' + req.body.name + '.\n\rEmail: ' + req.body.email + '.\n\rPhone: ' + req.body.phone + '.\n\rAddress: ' + req.body.address + '.\n\rMessage: ' + req.body.message)
 	}, function(error, success) {
